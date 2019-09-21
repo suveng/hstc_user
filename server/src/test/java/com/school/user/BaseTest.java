95050f4f55021386ebaf704c6ceaaf36f5032a3f
suveng <suveng@163.com>
2019-09-21 16:30:37 +0800

    --story=1000018 --user=苏文广 初始化后端

---
+++ server/src/test/java/com/school/user/BaseTest.java
@@ -0,0 +1,16 @@
+package com.school.user;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * description:
+ * @author suwenguang
+ * @date 2019-08-24
+ * @version 1.0.0
+ **/
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = UserServiceApplication.class)
+public class BaseTest {
+}
